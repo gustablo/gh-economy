83156899a008a0cc4f3ee077f52e56f2746eaded
gustablo <gpinheiro2001@gmail.com>
2022-10-13 00:20:56 -0300

    part 1 of bets real time flux

--- web/gh-economy/src/api/base.js
+++ web/gh-economy/src/api/base.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.VUE_APP_BASE_URL,
+    baseURL: process.env.VITE_API_ENDPOINT,
     headers: {
         authorization: localStorage.getItem('token'),
         'Content-Type': 'application/json',
